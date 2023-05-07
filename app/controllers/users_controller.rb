e72e14d2245cf2cafe986eb39b3403a1b75bab9e
EC2 Default User <ec2-user@ip-172-31-36-1.ap-northeast-1.compute.internal>
2023-05-07 07:53:40 +0000

    マイページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,23 @@ class UsersController < ApplicationController
     @user = current_user
   end
   
+  def edit
+    @user = current_user
+  end
+  
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to users_path(@user)
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :goal_calorie)
+  end
+  
 end
