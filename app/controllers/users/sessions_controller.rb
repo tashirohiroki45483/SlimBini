0fdcdf55a2b2f64cf54c422182bd9947c4af97ec
EC2 Default User <ec2-user@ip-172-31-36-1.ap-northeast-1.compute.internal>
2023-05-07 13:56:06 +0000

    マイページ作成

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,7 +3,7 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   
-  def guest_sign_in #ゲストログイン
+  def guest_sign_in #ゲストログインサインイン
     user = User.guest
     sign_in user
     redirect_to user_path(user), notice: 'guestuserでログインしました。'
@@ -24,7 +24,11 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  
+  def after_sign_in_path_for(resource) #サインイン後にマイページに遷移する
+    user_path(current_user)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,8 @@
 class UsersController < ApplicationController
-  
+   before_action :ensure_guest_user, only: [:edit]
+   
   def show
     @user = current_user
-  end
-  
-  def edit
-    @user = current_user
-  end
-  
-  def update
-    @user = current_user
-    if @user.update(user_params)
-      redirect_to users_path(@user)
-    else
-      render :edit
-    end
-  end
-  
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :goal_calorie)
   end
   
 end
