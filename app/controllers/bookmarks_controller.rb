7026edd964df2d1997c050304245d4c84e89f429
tashirohiroki45483 <126646683+tashirohiroki45483@users.noreply.github.com>
2023-05-15 12:16:18 +0000

    いいね・ブックマークの非同期通信化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,15 +2,15 @@ class BookmarksController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @menu = Menu.find(params[:menu_id])
     bookmark = current_user.bookmarks.build(menu_id: params[:menu_id])
     bookmark.save
-    redirect_to request.referer
   end
 
   def destroy
+    @menu = Menu.find(params[:menu_id])
     bookmark = Bookmark.find_by(user_id: current_user.id, menu_id: params[:menu_id])
     bookmark.destroy
-    redirect_to request.referer
   end
 
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,15 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @menu = Menu.find(params[:menu_id])
     like = current_user.likes.build(menu_id: params[:menu_id])
     like.save
-    redirect_to request.referer
   end
 
   def destroy
+    @menu = Menu.find(params[:menu_id])
     like = Like.find_by(user_id: current_user.id, menu_id: params[:menu_id])
     like.destroy
-    redirect_to request.referer
   end
-  
+
 end
