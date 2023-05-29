213aa4d7766f9d7a89cfbfacd9ece18dfad846e8
tashirohiroki45483 <126646683+tashirohiroki45483@users.noreply.github.com>
2023-05-29 10:35:12 +0000

    seeds

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
 
   def top
-    @menus = Menu.order(created_at: :desc).limit(3)
+    @menus = Menu.order(created_at: :desc).limit(3) #Menuの新着３件を取得する
   end
 
 end
